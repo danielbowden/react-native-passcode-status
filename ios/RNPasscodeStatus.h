523b67170f2f48d9078b715d09f7713d1e448dc3
Daniel Bowden <daniel.bowden@qantas.com.au>
2020-01-08 16:34:37 +1100

    Fix/modernise react imports

--- ios/RNPasscodeStatus.h
+++ ios/RNPasscodeStatus.h
@@ -7,9 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
-#import <RCTBridgeModule.h>
-#import "UIDevice+PasscodeStatus.h"
+#if __has_include("RCTBridgeModule.h")
+#import "RCTBridgeModule.h"
+#else
+#import <React/RCTBridgeModule.h>
+#endif
+#if __has_include("RCTUtils.h")
 #import "RCTUtils.h"
+#else
+#import <React/RCTUtils.h>
+#endif
+#import "UIDevice+PasscodeStatus.h"
 
 @interface RNPasscodeStatus : NSObject <RCTBridgeModule>
 
